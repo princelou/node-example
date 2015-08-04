e65b16320a5ea89c5e62afdf0fe32355596f8d61
caixiaojia <shawntsia@gmail.com>
2015-08-04 21:49:48 +0800

    consle.dir example

---
+++ console_dir/index.js
@@ -0,0 +1,9 @@
+//console.dir() µÄÊ¹ÓÃ
+var shawn = new Object();
+shawn.name = 'shawn';
+shawn.age = '21';
+shawn.show = function(){
+ return 'name:'+shawn.name+';age:'+shawn.age;
+}
+
+console.dir(shawn);
